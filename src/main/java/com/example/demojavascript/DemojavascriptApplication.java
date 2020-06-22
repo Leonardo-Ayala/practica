b92c189391aa832bc93963929429c94937545ae1
Leonardo <a20125980@pucp.pe>
2020-06-22 17:14:39 -0500

    commit

---
+++ src/main/java/com/example/demojavascript/DemojavascriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavascriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojavascriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojavascript/DemojavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojavascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojavascriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
